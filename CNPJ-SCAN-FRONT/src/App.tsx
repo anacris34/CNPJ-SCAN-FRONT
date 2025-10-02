3c978182c5b4db3023d86c15adb5d08322b7f2ed
Ana Cris Silva <anacrist60@gmail.com>
2025-10-02 17:36:43 -0300

    footer frist version

--- CNPJ-SCAN-FRONT/src/App.tsx
+++ CNPJ-SCAN-FRONT/src/App.tsx
@@ -1,12 +1,22 @@
-import { Button, Heading, VStack } from '@chakra-ui/react'
+import { Box, Button, Heading, VStack } from '@chakra-ui/react'
 import React from 'react';
 import Header from './components/ui/header';
+import Footer from './components/ui/footer'; 
 
 function App() {
   return (
-    <VStack>
-      <Header 
+     <VStack w="100%" minHeight="100vh" justifyContent="space-between">
+      
+     
+      <Header title="CNPJ Scan" />
+        
+      <Box flex="1" p={8}>
+   
+      </Box>
+
+      <Footer 
         title="CNPJ Scan" 
+        copyrightText="Grupo 3 NEXT"
       />
     </VStack>
   )
