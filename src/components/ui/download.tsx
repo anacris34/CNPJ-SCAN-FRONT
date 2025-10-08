54f79ea1a0cac5c595e0ba01db0931e34560d9ce
Pedro Neiva <pedrodeabn@gmail.com>
2025-10-08 10:35:23 -0300

    ...

--- src/components/ui/download.tsx
+++ src/components/ui/download.tsx
@@ -1,6 +1,6 @@
-import { Button, DownloadTrigger } from "@chakra-ui/react"
+import { Button, DownloadTrigger, Text } from "@chakra-ui/react";
 
-//const data = tabela backend
+const data = "[[1,2],[1,2]]";
 
 const DownloadCSV = () => {
   return (
@@ -10,8 +10,18 @@ const DownloadCSV = () => {
       mimeType="text/csv"
       asChild
     >
-      <Button variant="outline">Download CSV</Button>
+      <Button
+        variant="solid"
+        colorPalette="white"
+        boxShadow="md"
+        size="2xl"
+        rounded="md"
+      >
+        <Text color="#036DC5" textStyle="md" fontWeight="bold">
+          Download CSV
+        </Text>
+      </Button>
     </DownloadTrigger>
-  )
-}
+  );
+};
 export default DownloadCSV;
