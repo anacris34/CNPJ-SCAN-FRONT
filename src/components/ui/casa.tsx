ef6724d997a9f1902455ec375d49a4499ec9f3d6
Pedro Neiva <pedrodeabn@gmail.com>
2025-10-08 14:15:19 -0300

    att downloadpage

--- src/components/ui/casa.tsx
+++ src/components/ui/casa.tsx
@@ -2,29 +2,20 @@ import React from "react";
 import { IconButton, Flex, Link } from "@chakra-ui/react";
 import { IoHomeOutline } from "react-icons/io5";
 
-const Casa: React.FC = () => {
+const Home: React.FC = () => {
   return (
-    <Flex
-      direction="column"
-      align="center"
-      justify="center"
-      minH="100vh"
-      gap={4}
-    >
-      <Link href="/" _hover={{ textDecoration: "none" }}>
-        <IconButton
-          colorPalette="white"
-          aria-label="Ir para home"
-          boxShadow="md"
-          rounded="full"
-          variant="solid"
-          size="lg"
-        >
-          <IoHomeOutline color="#036DC5" />
-        </IconButton>
-      </Link>
-    </Flex>
+    <Link href="/" _hover={{ textDecoration: "none" }}>
+      <IconButton
+        aria-label="Ir para home"
+        boxShadow="md"
+        rounded="full"
+        variant="solid"
+        size="lg"
+      >
+        <IoHomeOutline color="#036DC5" />
+      </IconButton>
+    </Link>
   );
 };
 
-export default Casa;
+export default Home;
--- src/pages/DownloadPage.tsx
+++ src/pages/DownloadPage.tsx
@@ -3,20 +3,32 @@ import Home from "../components/ui/casa";
 import Tabela from "../components/ui/tabela";
 import DownloadCSV from "../components/ui/download";
 
+
 function Download() {
   return (
-    <VStack w="100%" minHeight="100vh" align="center">
-      <Box flexGrow={8} p={8} w="100%" textAlign="center">
-        <Heading size="3xl" mb={8}>
-          Converta PDF para EXCEL.
-        </Heading>
-        <Tabela />
-        <Flex align="center" justify="center" gap={10} mt={4}>
+    <VStack w="100%" minHeight="100vh" align="center" gap={10}>
+      <Header title="CNPJ Scan" />
+      <Box p={10} w="100%" textAlign="center" marginEnd={5}>
+        <Heading size="3xl">Converta PDF para EXCEL</Heading>
+        <Text mb="50px" fontSize="md" color="white">
+          Powerd by Cesar School.
+        </Text>
+        <Text mb="5px" fontSize="lg" fontWeight="bold" color="white">
+          Os dados foram convertidos
+        </Text>
+        <Center>
+          <TabelaDinamica />
+        </Center>
+
+        <Flex align="top-center" justify="center" gap={7} mt={2}>
           <Home />
           <DownloadCSV />
         </Flex>
       </Box>
-	  </VStack>
+	  	<Footer 
+        title="CNPJ Scan" 
+        copyrightText="Grupo 3 NEXT"	/>      
+    </VStack>
   );
 }
 
