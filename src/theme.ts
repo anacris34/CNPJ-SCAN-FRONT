09b4972fdcde3d77b91f6ac23423e664fa12d3bd
Pedro Neiva <pedrodeabn@gmail.com>
2025-10-09 16:37:07 -0300

    Teste com novo theme. Teste para remover o index.css e otimizar o theme para receber animaçoes e novas atts

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,40 @@
-export {extendTheme} from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
 
-export const theme = extendTheme({
-   config: {
-    initialColorMode: "dark"
+const theme = extendTheme({
+    config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false
+   },
+   styles: {
+    global: (props: any) => ({
+      html: {
+        height: "100%",
+        overflowX: "hidden",
+      },
+    config: {
+      initialColorMode: "dark"
    }
-})
+      body: {
+        height: "100%",
+        margin: 0,
+        padding: 0,
+        overflowX: "hidden",
+        color: "white",
+        fontFamily: "Arial, sans-serif",
+        bgGradient: mode(
+          "linear(to-b, #1D94F0 0%, #1D94F0 78%, #036DC5 100%)",
+          "linear(to-b, #1D94F0 0%, #1D94F0 78%, #036DC5 100%)"
+        )(props),
+      },
+      "#root, #app": {
+        height: "100%",
+      },
+      "input::placeholder": {
+        color: "white",
+      },
+    }),
+  },
+});
+
+export default theme;
